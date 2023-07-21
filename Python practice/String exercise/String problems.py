6f8a08a7dbd3bd0d71840606ccb477e7ee175d11
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2023-07-21 14:47:05 +0600

    string problems solved

--- Python practice/String exercise/String problems.py
+++ Python practice/String exercise/String problems.py
@@ -90,15 +90,201 @@ print("Digit: ", d)
 print("Char: ", c)
 """
 # Exercise 6: Create a mixed String using the following rules
+
+# Given:
+# s1 = "Abc"
+# s2 = "Xyz"
+# Expected Output:
+# AzbycX
+
+"""s1 = "Abc"
+s2 = "Xyz"
+
+# get string length
+s1_length = len(s1)
+s2_length = len(s2)
+
+# get length of a bigger string
+length = s1_length if s1_length > s2_length else s2_length
+result = ""
+
+# reverse s2
+s2 = s2[::-1]
+
+# iterate string 
+# s1 ascending and s2 descending
+for i in range(length):
+    if i < s1_length:
+        result = result + s1[i]
+    if i < s2_length:
+        result = result + s2[i]
+
+print(result)"""
+
+
 # Exercise 7: String characters balance Test
+
+"""
+def string_check(s1,s2):
+    flag = True
+
+    for char in s1:
+        if char in s2:
+            continue
+        else:
+            flag = False
+    return flag
+
+s1 = "Yn"
+s2 = "PYnative"
+flag = string_check(s1, s2)
+print("s1 and s2 are balanced:", flag)
+
+s1 = "Ynf"
+s2 = "PYnative"
+flag = string_check(s1, s2)
+print("s1 and s2 are balanced:", flag)
+"""
 # Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
+"""
+str1 = "Welcome to USA. usa awesome, isn't it?"
+sub_string = "USA"
+
+temp_str = str1.lower()
+count = temp_str.count(sub_string.lower())
+print("the usa count is:", count)
+"""
+
 # Exercise 9: Calculate the sum and average of the digits present in a string
+"""
+str1 = "PYnative29@#8496"
+
+list1 = []
+count = 0
+
+for char in str1:
+    if char.isdigit():
+        list1.append(int(char))
+        count+=1
+
+avg = sum(list1)/count
+print("Sum is:", sum(list1), "Average is ", avg)
+"""
+#Alternative solution:
+"""
+input_str = "PYnative29@#8496"
+total = 0
+cnt = 0
+for char in input_str:
+    if char.isdigit():
+        total += int(char)
+        cnt += 1
+
+# average = sum / count of digits
+avg = total / cnt
+print("Sum is:", total, "Average is ", avg)
+"""
+
 # Exercise 10: Write a program to count occurrences of all characters within a string
-# Exercise 11: Reverse a given string
+
+"""str1 = "Apple"
+
+char_dict = dict()
+
+for char in str1:
+    count = str1.count(char)
+    char_dict[char] = count
+print("Result:", char_dict)
+"""
+# Exercise 11: Reverse a given string . For many type of method "https://www.scaler.com/topics/reverse-a-number-in-python/"
+
+"""str2 = "Apple"
+print(''.join(reversed(str2)))"""
+
+#Alternative method:
+
+"""str1 = "Pynative"
+print(str1[::-1])"""
+
 # Exercise 12: Find the last position of a given substring
+
+"""str1 = "Emma is a data scientist who knows Python. Emma works at google."
+print("Original String is:", str1)
+index = str1.rfind("Emma")
+print("Last occurance of Emma starts at index:", index)
+"""
+
 # Exercise 13: Split a string on hyphens
+
+"""str1 = "Emma-is-a-data-scientist"
+sub_string = str1.split("-")
+for sub in sub_string:
+    print(sub)"""
+
 # Exercise 14: Remove empty strings from a list of strings
+
+"""str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+res_list = []
+for s in str_list:
+    if s:
+        res_list.append(s)
+print(res_list)"""
+
+
+#Alternative solution:
+"""str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+
+sub_string = list(filter(None, str_list))
+print(sub_string)"""
+
 # Exercise 15: Remove special symbols / punctuation from a string
+
+"""import string
+str1 = "/*Jon is @developer & musician"
+
+new_str = str1.translate(str.maketrans('', '', string.punctuation))
+print(new_str)"""
+
+#Alternative Method:
+
+"""import re
+
+str1 = "/*Jon is @develper & musician"
+
+res = re.sub(r'[^\w\s]','',str1)
+print(res)"""
+
 # Exercise 16: Removal all characters from a string except integers
+"""
+str1 = 'I am 25 years and 10 months old'
+
+res = ''.join([item for item in str1 if item.isdigit()])
+print(res)
+
+"""
 # Exercise 17: Find words with both alphabets and numbers
+"""
+str1 = "Emma25 is Data scientist50 and AI Expert"
+
+res = []
+temp = str1.split()
+
+for item in temp:
+    if any(char.isalpha() for char in item) and any(char.isdigit() for char in item):
+        res.append(item)
+    
+for i in res:
+    print(i)
+"""
 # Exercise 18: Replace each special symbol with # in the following string
+
+import string
+
+str1 = '/*Jon is @developer & musician!!'
+
+replace_char = "#"
+
+for char in string.punctuation:
+    str1 = str1.replace(char, replace_char)
+print(str1)
+
